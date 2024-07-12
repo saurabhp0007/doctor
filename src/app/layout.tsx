8d0dcb315ce2c072c02e5eb8b6aedb6cae89c7cb
saurabh kumar pandey <saurabhpanday21212@gmail.com>
2024-07-12 12:39:06 +0530

    change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import {cn} from '../../lib/utils'
+import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("min-h-screen bg-dark-300", inter.className)}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
     </html>
   );
 }
